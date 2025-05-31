fa3791b566484a3f228f5c33073b16042e6a91d8
Vasif07 <mvasif318@gmail.com>
2025-05-31 14:22:28 +0400

    jsts-first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+const getMenuItem = (arg: string): string => {
+  console.log("Receive item: " + arg);              
+  const result = "Received item: " + arg;            
+  console.log(result);                               
+  return arg;  
+}
+
+const newPages: number[] = [5,4,3,2,1]
+
+const menuItems: string[] = ["Каталог","Стройматериалы","Инструменты","Электрика","Интерьер и одежда"]
+
+const upperCaseMenuItems: string[] = menuItems.map(
+  (item:string) => {
+  return item.toUpperCase();
+  }
+)
+
+console.log(upperCaseMenuItems)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  isShowCatalog: boolean = false;
+
+  readonly headerItem1: "Главная" = "Главная";
+  readonly headerItem3: "Каталог" = "Каталог";
+  readonly aboutCompany: string =getMenuItem("О компании");
+
+  isShowImg: boolean = true;
+
+  readonly newPages: number[] = newPages;
+
+  isUpperCase: boolean = true;
+
+  menuItems: string[] = upperCaseMenuItems
+
+  changeMenuText() {
+    this.menuItems = upperCaseMenuItems.map(
+      item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    )
+  
+    this.isUpperCase = !this.isUpperCase
+  }
 }
+
+
